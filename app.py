28800ea78d901d40cb294e1661c12c9f631ef011
Ekeoma E. AGU <aguekeomae@gmail.com>
2023-08-29 15:48:56 +0100

    removed pillow import

--- app.py
+++ app.py
@@ -23,8 +23,6 @@ from components.metrics import (
     get_reference,
 )
 from streamlit_extras.app_logo import add_logo
-from PIL import Image
-import duckdb as db
 
 # ========= Page setup ======================
 sl.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
